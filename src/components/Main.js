968e1c36081c9dbb2e1eb34e40d52356aec7b0ab
EDT0804 <133602586+EDT0804@users.noreply.github.com>
2023-06-12 19:05:51 +0200

    -F minutori pamja ok

--- src/components/Main.js
+++ src/components/Main.js
@@ -48,45 +48,48 @@ render() {
                     <label className="label" htmlFor="sufsk_np">NGRITJA E PESHËS </label>
                     <div className="control">
                         <input className="input" type="number" pattern="[0-9]*" min="0" inputMode="numeric" id="sufsk_np" onChange={this.props.handleChange} placeholder="0" />
+                    </div>
                 </div>
-               </div>
-                    <div className="columns">
-                        <div className="column">
-                            <div className="field">
-                                <label className="label" htmlFor="sufsk_mi">MBËSHTETJE MBI PARAKRAHË</label>
-                                <div className="control">
-                                    <input className="input" type="number" pattern="[0-9]*" min="0" max="99" inputMode="numeric" id="sufsk_mi" onChange={this.props.handleChange} placeholder="00" />
-                                </div>
-                        </div>
-                        <div className="column">
-                            <div className="field">
-                                <label className="label" htmlFor="sufsk_se">&nbsp;</label>
-                                <div className="control">
-                                    <input className="input" type="number" pattern="[0-9]*" min="0" max="59" inputMode="numeric" id="sufsk_se" onChange={this.props.handleChange} placeholder="00" />
-                                </div>
-                        </div>
-                        <div className="columns">
-                            <div className="column">
-                            <div className="field">
-                                <label className="label" htmlFor="sufsk_min">2-km VRAPIM</label>
-                                <div className="control">
-                                    <input className="input" type="number" pattern="[0-9]*" min="0" max="99" inputMode="numeric" id="sufsk_min" onChange={this.props.handleChange} placeholder="00" />
-                                </div>
-                        </div>
-                        <div className="column">
-                            <div className="field">
-                                <label className="label" htmlFor="sufsk_sec">&nbsp;</label>
-                                <div className="control">
-                                    <input className="input" type="number" pattern="[0-9]*" min="0" max="59" inputMode="numeric" id="sufsk_sec" onChange={this.props.handleChange} placeholder="00" />
-                                </div>
+                <div className="columns">
+                    <div className="column">
+                        <div className="field">
+                            <label className="label" htmlFor="sufsk_mi">2-km VRAPIM</label>
+                            <div className="control">
+                                <input className="input" type="number" pattern="[0-9]*" min="0" max="99" inputMode="numeric" id="sufsk_mi" onChange={this.props.handleChange} placeholder="00" />
                             </div>
                         </div>
                     </div>
-              </div>
-              </div>
-              </div>
-              </div>
-              </div>
+                    <div className="column">
+                        <div className="field">
+                            <label className="label" htmlFor="sufsk_se">&nbsp;</label>
+                            <div className="control">
+                                <input className="input" type="number" pattern="[0-9]*" min="0" max="99" inputMode="numeric" id="sufsk_se" onChange={this.props.handleChange} placeholder="00" />
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column">
+                        <div className="field">
+                            <label className="label" htmlFor="sufsk_min">2-km VRAPIM</label>
+                            <div className="control">
+                                <input className="input" type="number" pattern="[0-9]*" min="0" max="99" inputMode="numeric" id="sufsk_min" onChange={this.props.handleChange} placeholder="00" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="field">
+                            <label className="label" htmlFor="sufsk_sec">&nbsp;</label>
+                            <div className="control">
+                                <input className="input" type="number" pattern="[0-9]*" min="0" max="99" inputMode="numeric" id="sufsk_sec" onChange={this.props.handleChange} placeholder="00" />
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </main>
         );
     }
