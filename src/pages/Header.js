b646295172de62cb2164b3e4c41fdda1e1c1e7d9
EDT0804 <133602586+EDT0804@users.noreply.github.com>
2023-06-12 20:52:31 +0200

    -f rikthimi i rrethit

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,9 +3,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="w3-container w3-padding-32 w3-center w3-black" id="ballina">
-      {/* Header content */}
-    </header>
+   <div></div>
   );
 }
 
